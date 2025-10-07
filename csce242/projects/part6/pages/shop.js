835756bf8ce861894517149346362e46b5c6eb95
r-vazrosa <rvazquezrosales8888@gmail.com>
2025-10-07 12:49:32 -0400

    testing fetch

--- csce242/projects/part6/pages/shop.js
+++ csce242/projects/part6/pages/shop.js
@@ -1,8 +1,33 @@
-const shopList = document.getElementById('shop');
+const shopList = document.getElementById("shop");
 
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(item => console.log(item.name));
-  })
-  .catch(err => console.error('Error loading JSON:', err));
+const getData = async() => {
+    const url = "r-vazrosa.github.io/csce242/json/shop.json";
+
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        data.forEach(item => console.log(item.name));
+
+        const shopItem = document.createElement('div');
+        shopItem.innerHTML = 
+        `
+            <div class="shop-item-description">
+                <img src="../images/120.ico">
+                <div>
+                    <h2>${item.name} ( +${item.value} / ${item.type} )</h2>
+                    <p>
+                        ${item.description}
+                    </p>
+                </div>
+            </div>
+            <div class="shop-item-purchase">
+                <img src="../images/865.ico">
+            </div>
+        `;
+    } catch (e) {
+        console.error("Error fetching data:", error);
+    }
+
+}
