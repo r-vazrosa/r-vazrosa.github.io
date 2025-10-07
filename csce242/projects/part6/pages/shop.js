331dd357b5ae821d690317d07f89150a451db5e0
r-vazrosa <rvazquezrosales8888@gmail.com>
2025-10-07 14:19:20 -0400

    finished project part 6

--- csce242/projects/part6/pages/shop.js
+++ csce242/projects/part6/pages/shop.js
@@ -8,27 +8,35 @@ const getData = async() => {
 
         const data = await response.json();
 
-        data.forEach(item => console.log(item.name));
+        data.forEach(item => {
+            console.log(item.name);
+            const shopItem = document.createElement('div');
+            shopItem.className = ((item.id % 2 == 0) ? "shop-item-alternate" : "shop-item");
+            const description = ((item.id % 2 == 0) ? "shop-item-description-alternate" : "shop-item-description")
 
-        const shopItem = document.createElement('div');
-        shopItem.innerHTML = 
-        `
-            <div class="shop-item-description">
-                <img src="../images/120.ico">
-                <div>
-                    <h2>${item.name} ( +${item.value} / ${item.type} )</h2>
-                    <p>
-                        ${item.description}
-                    </p>
+            shopItem.innerHTML = 
+            `
+                <div class=${description}>
+                    <img src="../images/item-img-${item.id}.png">
+                    <div>
+                        <h2>${item.name} ( +${item.value} / ${item.type} )</h2>
+                        <p>
+                            ${item.description}
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="shop-item-purchase">
-                <img src="../images/865.ico">
-            </div>
-        `;
-        shopList.appendChild(shopItem);
+                <div class="shop-item-purchase">
+                    <img src="../images/shop-purchase.png">
+                </div>
+            `;
+            shopList.appendChild(shopItem);
+        }) 
+
+        
     } catch (e) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching data:", e);
     }
 
 }
+
+getData();
